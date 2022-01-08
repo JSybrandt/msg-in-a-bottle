66c8a2fc60251f89144cf4b19f863b3111d8ca58
Justin Sybrandt <jsybrandt@pop-os.localdomain>
2022-01-08 17:44:34 -0500

    Adds login routine

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,60 @@
 from server import db, DATABASE_PATH
+from sqlalchemy.sql import func
+import util
+from datetime import datetime, timedelta
+
+VALID_LOGIN_ATTEMPT_DELTA = timedelta(minutes=5)
+TOKEN_VALID_DELTA = timedelta(days=10)
+
 
 class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String(120), unique=True, nullable=False)
+  email = db.Column(db.String(120), primary_key=True)
   name = db.Column(db.String(120), nullable=False)
+
+class PendingLoginRequest(db.Model):
+  email = db.Column(db.String(120), db.ForeignKey(User.email), primary_key=True)
+  secret_key = db.Column(db.String(10), nullable=False)
+  timestamp = db.Column(db.DateTime, nullable=False)
+
+class AccessToken(db.Model):
+  email = db.Column(db.String(120), db.ForeignKey(User.email), primary_key=True)
+  token = db.Column(db.String(util.ACCESS_TOKEN_LENGTH), primary_key=True)
+  timestamp = db.Column(db.DateTime, nullable=False)
+
+
+
+# Login
+
+def open_login_request(email):
+  """Returns a secret key that the user can use to login."""
+  print("prepare_login_attempt")
+  secret_key = util.generate_secret_key()
+  existing_query = db.session.query(PendingLoginRequest).filter(PendingLoginRequest.email == email)
+  if existing_query.first() is None:
+    db.session.add(PendingLoginRequest(email=email, secret_key=secret_key, timestamp=util.now()))
+  else:
+    existing_query.update(dict(secret_key=secret_key, timestamp=util.now()))
+  db.session.commit()
+  return secret_key
+
+def close_login_request(email, secret_key):
+  """Returns a token if the secret key is valid for the email."""
+  attempt_query = db.session.query(PendingLoginRequest).filter(
+    PendingLoginRequest.email == email,
+    PendingLoginRequest.secret_key == secret_key.upper(),
+    PendingLoginRequest.timestamp > util.now() - VALID_LOGIN_ATTEMPT_DELTA
+  )
+  if attempt_query.count() != 1:
+    raise ValueError(f"Login attempt failed for email: {email}")
+  attempt_query.delete()
+  token = util.generate_access_token()
+  db.session.add(AccessToken(email=email, token=token, timestamp=util.now()))
+  db.session.commit()
+  return token
+
+
+def put(*items):
+  for i in items:
+    db.session.add(i)
+  db.session.commit()
+
--- backend/routes.py
+++ backend/routes.py
@@ -1,22 +1,32 @@
-from server import app, send_mail
 from flask import request, jsonify
+import util
+import database
+import server
 
-@app.errorhandler(ValueError)
+@server.app.errorhandler(ValueError)
 def value_error(e):
   return jsonify(error=str(e)), 400
 
-@app.errorhandler(AssertionError)
+@server.app.errorhandler(AssertionError)
 def value_error(e):
   return jsonify(error=str(e)), 500
 
-@app.route("/login", methods=["POST"])
+@server.app.route("/login", methods=["POST"])
 def login():
-  send_mail("test subject", "test_body", ["nalta45@gmail.com"])
   if not request.is_json:
     raise ValueError("/login expected json-encoded post.")
   data = request.get_json()
-  if "email" not in data:
+  email = data.get("email")
+  if email is None:
     raise ValueError("/login expected 'email' parameter.")
-  content = validate_content(request_json["content"])
-  response_content = database.throw_in_the_void(content)
-  return jsonify(content=response_content)
+  secret_key = data.get("secret_key")
+  if secret_key is None:
+    new_key = database.open_login_request(email)
+    server.send_mail(
+      "Msg in a Bottle: Temporary Password",
+      f"Your temporary password is: {new_key}",
+      [email])
+    return jsonify(status="new-login")
+  else:
+    token=database.close_login_request(email, secret_key)
+    return jsonify(status="login-success", token=token)
